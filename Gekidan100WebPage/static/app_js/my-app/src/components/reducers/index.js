4cb15bc5d2a946b141e3f82898b892f0976c7fb4
河口怜和人 <kawaguchireiwajin@Mac-mini.local>
2020-10-02 22:07:41 +0900

    Reactファイルの削除

--- Gekidan100WebPage/static/app_js/my-app/src/components/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import ajax from './xmaHttpRequest'
-import toggle from './domAdmin'
-import axios from "./axios";
-
-export default combineReducers({ ajax, toggle, axios })
