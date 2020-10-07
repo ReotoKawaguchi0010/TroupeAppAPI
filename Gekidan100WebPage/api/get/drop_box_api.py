61f7cbd6df7cd8198d768261003b72771f1b4aaf
河口怜和人 <kawaguchireiwajin@Mac-mini.local>
2020-10-08 00:08:18 +0900

    dropbox api word

--- Gekidan100WebPage/api/get/drop_box_api.py
+++ Gekidan100WebPage/api/get/drop_box_api.py
@@ -33,6 +33,38 @@ class DropboxApi(dropbox.Dropbox):
         links = self.sharing_list_shared_links(path).links
         print(links)
 
+    def extract_text(self, node):
+        text = lxml.etree.tostring(node, encoding='utf-8').decode('utf-8')
+        text = re.sub('<w:rt>.*?</w:rt>', '', text)
+        text = re.sub('<.*?>', '', text)
+
+        return text
+
+    def get_word_file(self, path):
+        word_in_dbx = self.sharing_list_shared_links(path).links[0].url.replace('www.dropbox.com', 'dl.dropboxusercontent.com').split('?')[0]
+        req = requests.get(word_in_dbx)
+        with zipfile.ZipFile(io.BytesIO(req.content)) as z:
+            xmlns = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+            xml_str = z.read('word/document2.xml')
+            xml_str_style = z.read('word/styles.xml')
+            styles = xmltodict.parse(xml_str_style.decode('utf-8'))
+            default_font_size = int(styles['w:styles']['w:docDefaults']['w:rPrDefault']['w:rPr']['w:sz']['@w:val']) / 2
+            dom = lxml.etree.fromstring(xml_str)
+            text_nodes = dom.xpath('//w:p', namespaces=xmlns)
+            text_list = []
+            for text_node in text_nodes:
+                xml_dicts = dict(xmltodict.parse(lxml.etree.tostring(text_node, encoding='utf-8').decode('utf-8'))['w:p'])
+                font_size = default_font_size
+
+                for i in xml_dicts.keys():
+                    if i == 'w:r':
+                        if xml_dicts['w:r']['w:rPr'] is not None:
+                            font_size = int(xml_dicts['w:r']['w:rPr']['w:sz']['@w:val']) / 2
+                        break
+                text = self.extract_text(text_node)
+                text_list.append({'text': text, 'font_size': font_size})
+            return text_list
+
 
 
 
@@ -40,66 +72,12 @@ class DropboxApi(dropbox.Dropbox):
 
 
 if __name__ == '__main__':
-    def extract_text(node):
-        text = lxml.etree.tostring(node, encoding='utf-8').decode('utf-8')
-        text = re.sub('<w:rt>.*?</w:rt>', '', text)
-        text = re.sub('<.*?>', '', text)
-
-        return text
 
 
     dbx = DropboxApi(DROPBOX_TOKEN)
+    print(dbx.get_word_file('/test/Document.docx'))
     # for file_folder_list in dbx.file_folder_lists('/test'):
     #     print(file_folder_list)
-
-    #test = dbx.sharing_create_shared_link_with_settings('/test/Document.docx')
-    #word_in_dbx = dbx.sharing_list_shared_links('/test/test.txt').links[1].url.replace('www.dropbox.com', 'dl.dropboxusercontent.com').split('?')[0]+'/text.txt'
-    word_in_dbx = dbx.sharing_list_shared_links('/test/Document.docx').links[0].url.replace('www.dropbox.com', 'dl.dropboxusercontent.com').split('?')[0]
-    req = requests.get(word_in_dbx)
-    z = zipfile.ZipFile(io.BytesIO(req.content))
-    xmlns = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
-    xml_str = z.read('word/document2.xml')
-    xml_str_style = z.read('word/styles.xml')
-
-    styles = xmltodict.parse(xml_str_style.decode('utf-8'))
-    default_font_size = int(styles['w:styles']['w:docDefaults']['w:rPrDefault']['w:rPr']['w:sz']['@w:val'])/2
-
-
-
-
-
-    dom = lxml.etree.fromstring(xml_str)
-    text_nodes = dom.xpath('//w:p', namespaces=xmlns)
-    text_list = []
-    for text_node in text_nodes:
-        xml_dicts = dict(xmltodict.parse(lxml.etree.tostring(text_node, encoding='utf-8').decode('utf-8'))['w:p'])
-        font_size = default_font_size
-
-        for i in xml_dicts.keys():
-            if i == 'w:r':
-                if xml_dicts['w:r']['w:rPr'] is not None:
-                    font_size = int(xml_dicts['w:r']['w:rPr']['w:sz']['@w:val'])/2
-                break
-        print(font_size)
-        text = extract_text(text_node)
-        text_list.append({text: font_size})
-    print(text_list)
-
-
-
-
-
-    # for i in z.filelist:
-    #     with z.open(i.filename) as myfile:
-    #         myfile_dict = xmltodict.parse(myfile.read().decode('utf-8'))
-    # with z.open('word/document2.xml') as myfile:
-    #     test_file = xmltodict.parse(myfile.read().decode('utf-8'))
-    #     document_file = json.loads(json.dumps(test_file))['w:document']['w:body']
-    #     for k, v in document_file.items():
-    #         print(k)
-    #         print(v)
-    z.close()
-
 
 
 
