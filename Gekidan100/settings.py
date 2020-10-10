a2fa0b4e9b5bbe59a3cbd9b6948b3bd5aa2a45f5
河口怜和人 <kawaguchireiwajin@Mac-mini.local>
2020-10-11 06:35:55 +0900

    add dockerfile

--- Gekidan100/settings.py
+++ Gekidan100/settings.py
@@ -86,18 +86,18 @@ WSGI_APPLICATION = 'Gekidan100.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': PSQL_NAME,
-        'USER': PSQL_USER,
-        'PASSWORD': PSQL_PASS,
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': PSQL_NAME,
+    #     'USER': PSQL_USER,
+    #     'PASSWORD': PSQL_PASS,
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 
