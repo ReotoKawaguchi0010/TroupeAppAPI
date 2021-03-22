696deeadedb4f767480390b6e46cfbb8e67057a9
ReotoKawaguchi0010 <pnxde6mxu@i.softbank.jp>
2021-03-23 02:00:13 +0900

    ticket re

--- front/src/js/webPage/containers/contents.tsx
+++ front/src/js/webPage/containers/contents.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {useRouteMatch} from "react-router";
+import {Switch, useRouteMatch} from "react-router";
 import {Box, Button} from "@material-ui/core";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 
 
 import {MenuIcon} from "js/webPage/components/menu"
+import {VideoTicket} from "js/webPage/containers/video_ticket";
+import {RouteWithSubRoutes} from "js/routes/routes";
+
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         body: {
+            margin: '120px 0',
             padding: '0 7%',
         },
     })
@@ -17,22 +21,44 @@ const useStyles = makeStyles((theme: Theme) =>
 
 
 
+const Main = () => {
+    const classes = useStyles()
+    const {path} = useRouteMatch()
+    return (
+        <div className={classes.body}>
+            <Box>Contents</Box>
+            <div>
+                <div><Link to={`${path}/video`}><Button>Video</Button></Link></div>
+                <div><Link to={`${path}/music`}><Button>Music</Button></Link></div>
+            </div>
+        </div>
+    )
+}
+
+
 
 
 export const Contents = () => {
-    const classes = useStyles()
     const {path} = useRouteMatch()
+    const routes = [
+        {
+            path: `${path}/video`,
+            component: VideoTicket,
+        },
+        {
+            path: path,
+            component: Main,
+        }
+    ];
     console.log(path)
     return (
         <>
             <MenuIcon />
-            <div className={classes.body}>
-                <Box>Contents</Box>
-                <div>
-                    <div><Link to={`${path}/video`}><Button>Video</Button></Link></div>
-                    <div><Link to={`${path}/music`}><Button>Music</Button></Link></div>
-                </div>
-            </div>
+            <Switch>
+                {routes.map((route, i) => (
+                    <RouteWithSubRoutes key={i} {...route} />
+                ))}
+            </Switch>
         </>
     )
 }
