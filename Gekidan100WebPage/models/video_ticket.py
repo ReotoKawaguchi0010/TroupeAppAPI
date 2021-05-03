4dae27b1dcab1f9aa8ed90495628cd7a01aa69a3
ReotoKawaguchi0010 <pnxde6mxu@i.softbank.jp>
2021-05-04 04:57:06 +0900

    division transfer and paypal

--- Gekidan100WebPage/models/video_ticket.py
+++ Gekidan100WebPage/models/video_ticket.py
@@ -1,13 +1,16 @@
 import datetime
+import secrets
 
 from django.db import models
 
 from Gekidan100WebPage.utils.util import encode_sha256
 
 class VideoTicket(models.Model):
+    name = models.CharField(max_length=137)
     permit = models.BooleanField(null=True)
     payment_methods = models.CharField(max_length=256)
     mail_address = models.CharField(max_length=256)
+    phone_number = models.CharField(max_length=16)
     payment_id = models.CharField(max_length=256)
     payer_id = models.CharField(max_length=256)
     token = models.CharField(max_length=256)
@@ -17,15 +20,72 @@ class VideoTicket(models.Model):
     joint = models.CharField(max_length=256)
 
 
+    def create(self, data: dict):
+        if self.has_need_payment_data(data):
+            self.__select_payment_methods_create(data)
+
     def permit_check(self, payment_methods):
         if payment_methods == 'paypal':
             return True
         return False
 
-    def create(self, payment_methods, mail_address, payment_id, payer_id, token):
+    def has_need_payment_data(self, data):
+        if data.keys() >= {
+            'name',
+            'payment_methods',
+            'mail_address',
+            'phone_number',
+            'payment_id',
+            'payer_id',
+            'token'
+        } or data.keys() >= {
+            'name',
+            'payment_methods',
+            'mail_address'
+            'phone_number',
+        }:
+            return True
+        return False
+
+    def __select_payment_methods_create(self, data):
+        if data['payment_methods'] == 'paypal':
+            return self.__paypal_create(name=data['name'],
+                                        payment_methods=data['payment_methods'],
+                                        mail_address=data['mail_address'],
+                                        phone_number=data['phone_number'],
+                                        payment_id=data['payment_id'],
+                                        payer_id=data['payer_id'],
+                                        token=data['token'])
+        elif data['payment_methods'] == 'transfer':
+            return self.__transfer_create(name=data['name'],
+                                          payment_methods=data['payment_methods'],
+                                          mail_address=data['mail_address'],
+                                          phone_number=data['phone_number'])
+        return None
+
+
+    def __transfer_create(self, name, payment_methods, mail_address, phone_number):
+        self.name = name
         self.permit = self.permit_check(payment_methods)
         self.payment_methods = payment_methods
         self.mail_address = mail_address
+        self.phone_number = phone_number
+        self.payer_id = encode_sha256(name)
+        self.payment_id = encode_sha256(encode_sha256(name) + encode_sha256(payment_methods))
+        self.token = secrets.token_hex()
+        self.payment_id_hash = encode_sha256(self.payment_id)
+        self.payer_id_hash = encode_sha256(self.payer_id)
+        self.timestamp = str(datetime.datetime.now().timestamp())
+        self.joint = encode_sha256(encode_sha256(self.timestamp) + self.payer_id + str(self.permit))
+        self.save()
+        return
+
+    def __paypal_create(self, name, payment_methods, mail_address, phone_number, payment_id, payer_id, token):
+        self.name = name
+        self.permit = self.permit_check(payment_methods)
+        self.payment_methods = payment_methods
+        self.mail_address = mail_address
+        self.phone_number = phone_number
         self.payer_id = payer_id
         self.payment_id = payment_id
         self.token = token
--- Gekidan100WebPage/tests/test_models.py
+++ Gekidan100WebPage/tests/test_models.py
@@ -1,15 +1,37 @@
+
+
 from django.test import TestCase
 
 from Gekidan100WebPage.models.video_ticket import VideoTicket
 from Gekidan100WebPage.models.performance.script import Script
 
 
+
+TEST_PAYMENT_ID = 'PAYID-MCIEGCI6GX50624X8279010B'
+TEST_PAYER_ID = 'ZQFR8L8CNFDC6'
+TEST_TOKEN = 'EC-0XH30116YU841021E'
+
+
+
+
 class VideoTicketModelTests(TestCase):
     def setUp(self):
+        data = {
+            'name': '河口 怜和人',
+            'payment_methods': 'transfer',
+            'mail_address': 'hello.jp',
+            'phone_number': '000-0000-0000',
+            'payment_id': '6436nio532hiohi3',
+            'payer_id': 'testtes53798hofas3',
+            'token': 'gsgoiifaf',
+        }
+
         video_ticket = VideoTicket()
-        video_ticket.create(payment_methods='transfer',
-                                   mail_address='test.jp', payment_id='6436nio532hiohi3',
-                                   payer_id='testtes53798hofas3', token='tokengdsgjio2426')
+
+        video_ticket.create(data)
+
+
+
 
 
 
@@ -18,12 +40,20 @@ class VideoTicketModelTests(TestCase):
 
         for video_ticket in video_tickets:
             print(video_ticket.id)
+            print(video_ticket.name)
+            print(video_ticket.mail_address)
+            print(video_ticket.payment_methods)
+            print(video_ticket.payer_id)
+            print(video_ticket.phone_number)
 
 
-        return 'hello'
+        self.assertEqual(len(video_tickets)>=1, True)
 
 class ScriptModelTests(TestCase):
     def setUp(self):
+        script = Script(script='hello')
+        script.save()
+
         script = Script(script='test')
         script.save()
 
